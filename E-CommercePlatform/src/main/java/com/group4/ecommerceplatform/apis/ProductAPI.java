a0d564c0e69af6644b8a2e1556e0d1953480c436
Minh Lam <qm02192803@gmail.com>
2026-01-17 16:35:42 +0700

    Create: add base structure

--- E-CommercePlatform/src/main/java/com/group4/ecommerceplatform/apis/ProductAPI.java
+++ E-CommercePlatform/src/main/java/com/group4/ecommerceplatform/apis/ProductAPI.java
@@ -1,8 +1,18 @@
 package com.group4.ecommerceplatform.apis;
 
+import com.group4.ecommerceplatform.dto.product.ProductDTO;
+import com.group4.ecommerceplatform.entities.Product;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductAPI {
-
+    @PostMapping("/admin/products")
+    public ResponseEntity<?> createProduct(@RequestBody @Valid ProductDTO dto)
+    {
+        return null;
+    }
 }
