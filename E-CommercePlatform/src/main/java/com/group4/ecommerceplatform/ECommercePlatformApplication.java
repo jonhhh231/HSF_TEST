c6514a8c9512dc72803d4b0a8315a28ad0863f25
jonhhh231 <tnguyndatnguyen251@gmail.com>
2026-01-17 13:38:40 +0700

    add

---
+++ E-CommercePlatform/src/main/java/com/group4/ecommerceplatform/ECommercePlatformApplication.java
@@ -0,0 +1,14 @@
+package com.group4.ecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePlatformApplication.class, args);
+    }
+
+
+}
